5eae777b26216b2f5c7e5815bc874c0da693e645
Lightnet <darkneter@gmail.com>
2022-04-07 22:22:43 -0700

    divider right add, placeholder object.

---
+++ src/components/three/entitycreature/EntityPawn.vue
@@ -0,0 +1,37 @@
+<script>
+//Testing
+// https://vuejs.org/guide/typescript/overview.html#general-usage-notes
+// might need to create class from defineComponent 
+/*
+  character:
+    - player and A.I movement and actions
+    - stats health points
+    - equipment
+    - alive/death
+    - camera
+*/
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  props: {
+    name: String,
+    //msg: { type: String, required: true }
+  },
+  data() {
+    return {
+      //count: 1
+    }
+  },
+  mounted() {
+    //this.name // type: string | undefined
+    //this.msg // type: string
+    //this.count // type: number
+  }
+})
+
+</script>
+<template>
+
+</template>
--- src/components/three/ui/EditorLayoutTest.vue
+++ src/components/three/ui/EditorLayoutTest.vue
@@ -20,6 +20,10 @@ const rightX = ref(0);
 const isDrag = ref(false);
 const positions = reactive({clientX:0,clientY:0,movementX:0,movementY:0,})
 
+const isDragRIght = ref(false);
+const rightBar = ref();
+const rightSideBar = ref();
+
 console.log(document)
 
 function leftBarDown(event){
@@ -49,13 +53,11 @@ function leftBarMove(event){
   leftSideBar.value.style.width = leftX.value + 'px'
   contentCenter.value.style.left = leftX.value + 4 + 'px'
   //console.log(leftSideBar.value.style.width)
-  //console.log(contentCenter.value.style.width)
   //console.log(contentCenter.value)
   //console.log(contentCenter.value.clientHeight)
   //console.log(contentCenter.value.clientWidth)
   //contentCenter.value.style.width = (leftWidth.value - leftX.value) + 'px';
-  //contentCenter.value.style.width = (leftWidth.value +  - leftX.value ) + 'px';
-  let widthLeft = parentWidth.value - 208;
+  let widthLeft = parentWidth.value - (leftBar.value.clientWidth + 8);
   contentCenter.value.style.width = (widthLeft - leftX.value ) + 'px';
 
 
@@ -66,6 +68,34 @@ function leftBarUp(event){
   isDrag.value=false;
   console.log("up?")
 }
+
+function rightBarDown(event){
+  isDragRIght.value=true;
+  positions.clientX = event.clientX
+  positions.clientY = event.clientY
+}
+function rightBarMove(event){
+  if(isDragRIght.value==false){
+    return;
+  }
+  positions.movementX = positions.clientX - event.clientX
+  positions.movementY = positions.clientY - event.clientY
+  positions.clientX = event.clientX
+  positions.clientY = event.clientY
+  //bar divider
+  rightX.value = (rightBar.value.offsetLeft - positions.movementX);
+  rightBar.value.style.left = rightX.value + 'px';
+  //panel
+  rightSideBar.value.style.left = (rightX.value + 8) + 'px';
+  rightSideBar.value.style.width = (rightBar.value.parentNode.clientWidth - rightX.value - 8)  + 'px'
+  //content
+  let cwidth = rightX.value - contentCenter.value.offsetLeft;
+  contentCenter.value.style.width = cwidth + 'px';
+  
+}
+function rightBarUp(event){
+  isDragRIght.value=false;
+}
 </script>
 <template>
   <div class="econtainer">
@@ -75,11 +105,14 @@ function leftBarUp(event){
     <div ref="contentCenter" class="contentcenter">
       Middle
     </div>
-    <div class="sidebarright">
+    <div ref="rightSideBar" class="sidebarright">
       Right
     </div>
     <div ref="leftBar" class="barleft" @mousedown="leftBarDown" @mousemove="leftBarMove" @mouseup="leftBarUp">
       bar
+    </div>
+    <div ref="rightBar" class="barright" @mousedown="rightBarDown" @mousemove="rightBarMove" @mouseup="rightBarUp">
+      RIGHT bar
     </div>
   </div>
 </template>
@@ -91,6 +124,7 @@ function leftBarUp(event){
   background-color: darkgray;
 }
 .sidebarleft{
+  cursor: default; /*Will make the cursor not point.*/
   position: absolute;
   top:20px;
   left: 0px;
@@ -109,6 +143,7 @@ function leftBarUp(event){
   z-index: 2;
 }
 .contentcenter{
+  /*cursor: none;*/
   position: absolute;
   top:20px;
   left: 208px;
@@ -117,11 +152,23 @@ function leftBarUp(event){
   background-color: antiquewhite;
 }
 .sidebarright{
+  /*cursor: none; */
   position: absolute;
   top:20px;
   right: 0px;
   width: 200px;
   height:calc(100% - 40px);
   background-color: gray;
+}
+
+.barright{
+  cursor:col-resize;
+  position: absolute;
+  top:20px;
+  left: calc(100% - 200px);
+  height:calc(100% - 40px);
+  width: 8px;
+  background-color: black;
+  z-index: 2;
 }
 </style>
